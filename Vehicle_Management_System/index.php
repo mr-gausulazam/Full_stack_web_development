c851d60944c6bf796c887f6e5f459ca6050ef430
Md. Gausul Azam <34084602+mr-gausulazam@users.noreply.github.com>
2025-09-04 20:36:18 +0600

    Update index.php

--- Vehicle_Management_System/index.php
+++ Vehicle_Management_System/index.php
@@ -1,1 +1,26 @@
 
+<?php
+$vehicleManager = new VehicleManager('data/vehicles.json');
+$vehicles = $vehicleManager->getVehicles();
+?>
+<div class="container">
+    <h1>Vehicles</h1>
+    <a href="views/add.php" class="btn btn-primary">Add New Vehicle</a>
+    <div class="row">
+        <?php foreach ($vehicles as $index => $vehicle): ?>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="<?php echo htmlspecialchars($vehicle['image']); ?>" class="card-img-top" alt="vehicle image">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo htmlspecialchars($vehicle['name']); ?></h5>
+                        <p class="card-text">Type: <?php echo htmlspecialchars($vehicle['type']); ?></p>
+                        <p class="card-text">Price: $<?php echo htmlspecialchars($vehicle['price']); ?></p>
+                        <a href="views/edit.php?id=<?php echo $index; ?>" class="btn btn-warning">Edit</a>
+                        <a href="views/delete.php?id=<?php echo $index; ?>" class="btn btn-danger">Delete</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
