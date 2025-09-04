229012abd0e6b9c5941bfdff8775e290c4c59553
Md. Gausul Azam <34084602+mr-gausulazam@users.noreply.github.com>
2025-09-04 20:38:40 +0600

    Create VehicleActions.php

---
+++ Vehicle_Management_System/VehicleActions.php
@@ -0,0 +1,8 @@
+
+interface VehicleActions {
+    public function addVehicle($data);
+    public function editVehicle($id, $data);
+    public function deleteVehicle($id);
+    public function viewVehicle($id);
+    public function getVehicles();
+}
